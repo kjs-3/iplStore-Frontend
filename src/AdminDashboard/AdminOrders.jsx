398deda736864267e1760167d5c1f57288f5f1b2
kjs-3 <justaddsj@gmail.com>
2026-09-07 01:04:40 +0530

    Update AdminOrders.jsx modified get axios

--- src/AdminDashboard/AdminOrders.jsx
+++ src/AdminDashboard/AdminOrders.jsx
@@ -11,7 +11,7 @@ const AdminOrders = () => {
     const fetchorders = async () => {
         setloading(true);
         try {
-            const res = await axios.get(`https://iplstore-backend.onrender.com/api/admin/getallorders`, { withCredentials: true })
+            const res = await axios.get(`https://iplstore-backend.onrender.com/api/admin/admin/getallorders`, { withCredentials: true })
             setorders(res.data)
             console.log(res.data)
         }
