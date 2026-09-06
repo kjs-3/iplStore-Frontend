ba9f643f2670e943d7149986d3e0f229b6af0a44
kjs-3 <justaddsj@gmail.com>
2026-09-06 19:08:02 +0530

    Update Products.jsx updated backend url in image

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -187,7 +187,7 @@ const Products = () => {
                                     <>
                                         <div key={product.productId} className={styles.productcard}>
                                             <div className={styles.imgbox} onClick={() => nav(`/ products / ${product.productId} `)}>
-                                                <img src={`http://localhost:8080${product.imageUrl}`} alt={product.name}
+                                                <img src={`https://iplstore-backend.onrender.com${product.imageUrl}`} alt={product.name}
                                                     className={styles.productimg} onError={(e) => e.target.style.display = 'none'} />
                                                 < span className={styles.catelabel} >
                                                     {product.categoryName}
