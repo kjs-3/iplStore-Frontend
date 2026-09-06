a862337dfae9996c788da326c778bacf1445b555
kjs-3 <justaddsj@gmail.com>
2026-09-06 15:36:33 +0530

    Update Register.jsx added backend url

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -32,7 +32,7 @@ const Register = () => {
             return;
         }
         try {
-            const res = await axios.post('http://localhost:8080/api/auth/register', {
+            const res = await axios.post(`https://iplstore-backend.onrender.com/api/auth/register', {
                 userName, userEmail, userPassword,
                 userPhonenumber
             });
